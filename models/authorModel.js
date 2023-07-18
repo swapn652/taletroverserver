06c6476054d4b9173420c1ff2a176dce1411db84
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-07-18 20:52:34 +0530

    created bookModel and authorModel

---
+++ models/authorModel.js
@@ -0,0 +1,26 @@
+// authorModel.js
+
+const mongoose = require('mongoose');
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  books: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+  }],
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+module.exports = Author;
---
+++ models/bookModel.js
@@ -0,0 +1,23 @@
+// bookModel.js
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author',
+    required: true,
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
