d3f0a1d65c581b21c6da31e8d1602a494473b94b
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-07-18 21:30:22 +0530

    created post request for books

--- routes/route.js
+++ routes/route.js
@@ -13,22 +13,42 @@ router.get('/yo', async(req, res) => {
 router.get('/books', async (req, res) => {
     try {
       const books = await Book.find();
-      res.status(200).json(books);
+      res.status(201).json(books);
     } catch (error) {
       res.status(500).json({ error: 'Failed to retrieve books' });
     }
   });
 
 // GET route for getting a specific book by ID
-app.get('/books/:id', async (req, res) => {
+router.get('/books/:id', async (req, res) => {
     try {
       const book = await Book.findById(req.params.id);
       if (!book) {
         return res.status(404).json({ error: 'Book not found' });
       }
-      res.status(200).json(book);
+      res.status(201).json(book);
     } catch (error) {
       res.status(500).json({ error: 'Failed to retrieve the book' });
+    }
+  });
+  
+
+// POST route for adding a new book
+router.post('/addBook', async (req, res) => {
+    try {
+      const { title, description, author } = req.body;
+  
+      const newBook = new Book({
+        title,
+        description,
+        author,
+      });
+  
+      const savedBook = await newBook.save();
+  
+      res.status(201).json(savedBook);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to add the book' });
     }
   });
   
